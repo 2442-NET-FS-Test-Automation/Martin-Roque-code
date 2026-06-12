539a92bae7ea40a8fbe19bf0a4d97351bcde5571
Martin Roque Sandoval <Martin-Roque>
2026-06-12 09:01:45 -0600

    Last updates from first class

---
+++ week-1/LibraryKata.App/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace LibraryKata.App; //Logical container for related code files
+
+public class Program
+{
+    
+
+    //main method
+    public static void Main()
+    {
+        DataTypesAndOperatos();
+    }
+
+    private static void DataTypesAndOperatos()
+    {
+        Console.WriteLine("== Data Types and Operators ==");
+
+        int copies = 3;
+        double lateFee = 3.25;
+        bool isMember = true;
+        char shelf = 'A';
+        string title = "Clean code";
+
+        //Operators
+        string user = "Jon";
+        int total = copies * 2; //arithmetic operator
+        bool isEnough = total > 4; //comparison operator
+
+        bool exactlySix = total == 6; //equality operator
+
+        bool lendable = isMember && isEnough; //logical operator
+
+        Console.WriteLine(title + " has been checked by " + user);
+
+
+        //String interpolation
+        Console.WriteLine($"{title} on shelf: {shelf}: {copies} copies, fee: {lateFee}");
+
+        total += 1; //assignment operator
+    }
+}
