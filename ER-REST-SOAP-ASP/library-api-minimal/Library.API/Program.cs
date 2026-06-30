87d023401f949e0071dd8feae94058f1fce0edb5
Martin Roque Sandoval <Martin-Roque>
2026-06-30 13:55:11 -0600

    API endpoints and LINQ

--- ER-REST-SOAP-ASP/library-api-minimal/Library.API/Program.cs
+++ ER-REST-SOAP-ASP/library-api-minimal/Library.API/Program.cs
@@ -9,8 +9,33 @@ var conn_string = "Server=localhost,1433;Database=LibraryMinimalDb;User ID=sa;Pa
 //Tell the builder to use our LibraryDbContext with the connection string above
 builder.Services.AddDbContext<LibraryDbContext>(options => options.UseSqlServer(conn_string));
 
+//Swagger added to builder
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//App area
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
+//Endpoint area
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/inventory", async (LibraryDbContext db) =>
+{
+    return await db.Inventory.ToListAsync();
+});
+
+//Using LINQ - Language Integrated Query
+//LINQ is a library that just lets us query collections - Logic based from SQL DQL
+app.MapGet("/inventory/by-value", (LibraryDbContext db) =>
+{
+    return db.Inventory.Include(i => i.Product)
+    .GroupBy(i => i.CurrentStock >= 5 ? "well-stocked" : "low")
+    .Select(g => new { tier = g.Key, count = g.Count(), units = g.Sum(i => i.CurrentStock) })
+    .ToList();
+});
+
+//Where file ends
 app.Run();
