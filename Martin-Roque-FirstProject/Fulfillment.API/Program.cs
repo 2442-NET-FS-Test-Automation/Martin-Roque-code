3dc813d7137f1a3b79d2a1fe6bdf84b83956598b
Martin Roque Sandoval <Martin-Roque>
2026-07-06 12:04:05 -0600

    Adding Swagger

--- Martin-Roque-FirstProject/Fulfillment.API/Program.cs
+++ Martin-Roque-FirstProject/Fulfillment.API/Program.cs
@@ -1,5 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello Revature!");
 
